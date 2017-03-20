ad9d6c72045a6e8a1b176394ef9f8a47c5d7f95c
f4tp <mr_treadwell@live.co.uk>
2017-03-20 09:25:55 +0000

    Changes to Patient(): lists inside now store the key of doctors and appointments, not restore the objects themselves. Objects can be retrieved from the composite by the key if needed

--- src/pkg15070779drsappsystem/Patient.java
+++ src/pkg15070779drsappsystem/Patient.java
@@ -39,7 +39,7 @@ public class Patient extends SystemUserComponent {
         this.newAppinstance = new Appointment (Name, appDate, appTime, drWith);
         
         this.lstPatientApps.add(newAppinstance.getAppUniqueKey());
-        //added again
+        //added again again
     }
     
     
