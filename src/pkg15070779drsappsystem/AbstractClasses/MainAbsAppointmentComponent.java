01a195929c2050f01ff13520414ad91c7ee3a1c9
f4tp <mr_treadwell@live.co.uk>
2017-04-11 14:50:30 +0100

    Spec & param comments started to be added

--- src/pkg15070779drsappsystem/AbstractClasses/MainAbsAppointmentComponent.java
+++ src/pkg15070779drsappsystem/AbstractClasses/MainAbsAppointmentComponent.java
@@ -16,17 +16,34 @@ public abstract class MainAbsAppointmentComponent {
   
     //no constructor as it is an abstract class
     
-    //generates a uniqueKey for each appointment created
+   
+    //example spec comment
+    /**
+   * /instance method, generates a uniqueKey for each appointment created
+   * x+yi. 
+   * @param x The real part of the complex number. 
+   * @param y The imaginary part of the complex number. 
+   */
+    
+    
+
+    /**
+   * /instance method; generates a uniqueKey for each appointment created.
+   */
     public String generateAppUniqueKey(){
         int noExistApps = mapAppointments.size() + 1;
         return "Appointment" + Integer.toString(noExistApps);
     }
     
-    
+   /**
+   * /instance method; puts the appointment object  in the map.
+   */
     public static void setPutInMap(String uKey, MainAppointment passedIn){
          mapAppointments.put(uKey, passedIn);
     }
-    
+     /**
+   * /instance method; pass this routine the unique appointment number and it will return the appointment object.
+   */
     public  static MainAppointment getAppointment(String uniqueKey){
         return mapAppointments.get(uniqueKey);
     }
--- src/pkg15070779drsappsystem/Interfaces/MainIntAbsUserComponent.java
+++ src/pkg15070779drsappsystem/Interfaces/MainIntAbsUserComponent.java
@@ -3,7 +3,8 @@ public abstract interface MainIntAbsUserComponent {
     
     //all user classes must implement these in order to encapsualte properly to 
     //keep good design patterns... this doesn't matter so much in my program but
-    //going forward it might when the program becomes larger
+    //going forward it might when the program becomes larger... and a new type of user might want to be
+    //created which uses different routines
     String getUserName();
     String getFirstName();
     String getSurname();
