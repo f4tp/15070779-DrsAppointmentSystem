d0f9fe4aed0f6722e7e39d7cfc7c3a7197904641
f4tp <mr_treadwell@live.co.uk>
2017-04-14 11:52:25 +0100

    Amend Appointment - now finished

--- src/pkg15070779drsappsystem/ActionListeners/ActListAmendAppointment.java
+++ src/pkg15070779drsappsystem/ActionListeners/ActListAmendAppointment.java
@@ -58,17 +58,25 @@ public class ActListAmendAppointment implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
         //these are the objects that are needed to interact with an appointment when changing it
         //only the Doctor and appointment will need updating
-        MainAppointment currentAppointmentInst;
-        MainDoctor currentDoctorInst;
-        MainPatient currentPatientInst;
+        MainAppointment currentAppointmentInst = MainAbsAppointmentComponent.getAppointment(AppKeyIn.getText());
+        MainDoctor currentDoctorInst = (MainDoctor) MainAbsSystemUserComponent.getSystemUserComponent(drUserNameIn.getText());
+        //MainPatient currentPatientInst;
         
+        //get the date and time of original appointment as a LDT object
+       LocalDateTime currentDateTimeOfInst = currentAppointmentInst.getAPPDateAndTime();
+       //get the date and time of the proposed appointment as a LDT object - if it needs to change
+        LocalDateTime newDateTimeOfApp = MainAbsScheduling.getConvStringToDateTime(cmbDayIn.getSelectedItem().toString() + cmbMonthin.getSelectedItem().toString() +cmbYearIn.getSelectedItem().toString() + cmbTimeIn.getSelectedItem().toString());
+     
+                           
+                           
+                      
         
         System.out.println("woo hoo" + cmbDayIn.getSelectedItem());
         System.out.println("woo hoohhh" + cmbTimeIn.getSelectedItem());
         JPanelAppAmendSecView amendAppFormInst = JPanelAppAmendSecView.getInstance();
         
         
-        //if false, just display a message sayign no updates found:
+        //if false, just display a message saying no updates found:
         if (amendAppFormInst.getFormUpdatedNotDateTime() == false && amendAppFormInst.getFormUpdatedDateTime() == false
                 && amendAppFormInst.getFormUpdatedNotDateTimeButAppAndDoc() == false){
             JOptionPane.showMessageDialog (null,
@@ -76,128 +84,106 @@ public class ActListAmendAppointment implements ActionListener {
                 "No Changes Made",
                 JOptionPane.ERROR_MESSAGE);
         }
-        
-        //if the form has been altered in any way, one of two variables will be set at true. One involves teh date and time being 
-        //interacted with which means more objects are involved and is more complicated. One involves just the appointment
-        //object being interacted with
-        
-        //program needs updating because somethign on the form other than the date and time has been updated
-        //this means only the appointment needs updating
-        if (amendAppFormInst.getFormUpdatedNotDateTime() == true){
-            //get the appointment, patient and doctor out of the map
-            //lazy instantiation
-            currentAppointmentInst= MainAbsAppointmentComponent.getAppointment(AppKeyIn.getText());
-            
-             //set appointment attended status
-            currentAppointmentInst.setAppAttendSec(attendedIn.isSelected());
-                     
-            //set appointment missed status
-            currentAppointmentInst.setAppMissed(missedIn.isSelected());
-            //set any new symptoms
-            currentAppointmentInst.setSymptoms(symptomsIn.getText());
-        
-          //put the appointment back in the map
-           // MainAbsAppointmentComponent.setPutInMap(AppKeyIn.getText(), currentAppointmentInst);
-           
-           callPromptAppupdated();
-           
-           //NTDgo back to patient record screen
-                     JFrameSecretaryMenu SecMenuRef = JFrameSecretaryMenu.getInstance();
-                    SecMenuRef.setSecSouthBorderString("DisplayPatientDetails");
-
+        //this means at least one change has been made on teh user form but a change to teh appointment might 
+        //not be made - if teh appointment the person wan'ts is not available:
+        //the test is if the date and time has changed and teh new time is not available... display error message
+        else if (amendAppFormInst.getFormUpdatedDateTime() == true && currentDoctorInst.getDocsAvailableAppointments().contains(newDateTimeOfApp) == false){
+            JOptionPane.showMessageDialog (null,
+                                                            "The doctor is busy on this date and time, please select an alternative date",
+                                                            "Appointment date & time Not available",
+                                                            JOptionPane.ERROR_MESSAGE);
         }
         
-        
-        
-        //this means the app has been cancelled, it needs to be marked as cancelled, and the doctor's schdule lists
-        //need updating (the appointment time taking out of theri taken list, and putting back in the available
-        //list
-        if(amendAppFormInst.getFormUpdatedNotDateTimeButAppAndDoc() == true){
-            
-            //lazy instantiation
-            currentAppointmentInst= MainAbsAppointmentComponent.getAppointment(AppKeyIn.getText());
-            currentDoctorInst  = (MainDoctor) MainAbsSystemUserComponent.getSystemUserComponent(drUserNameIn.getText());
-            //currentPatientInst = (MainPatient) MainAbsSystemUserComponent.getSystemUserComponent(patUserNameIn.getText());
-         
-              //set appointment cancelled status
-            currentAppointmentInst.setAppCancelled(cancelledIn.isSelected());
-            
-            //get the date and time of appointment as an object
-            LocalDateTime currentDateTimeOfInst = currentAppointmentInst.getAPPDateAndTime();
-            
-            //remove the appointment time from the doctors appointment time staken list
-            currentDoctorInst.setRemoveLDTFromAppTimesTakenList(currentDateTimeOfInst);
-            
-            //add the appointment time to the doctors appointments available list
-            currentDoctorInst.setAddLDTToAppTimesAvailableList(currentDateTimeOfInst);
-   
-            //*appointment still stays on file so no further action
-     
-        }
-        //this means that the date and time has changed, it is amore complicated routine involving more objects
-        if(amendAppFormInst.getFormUpdatedDateTime() == true){
-             //lazy instantiation
-            currentAppointmentInst= MainAbsAppointmentComponent.getAppointment(AppKeyIn.getText());
-            currentDoctorInst  = (MainDoctor) MainAbsSystemUserComponent.getSystemUserComponent(drUserNameIn.getText());
-            
-            
-            //get the date and time of original appointment as a LDT object
-            LocalDateTime currentDateTimeOfInst = currentAppointmentInst.getAPPDateAndTime();
-            
-            //get the date and time of the proposed appointment as a LDT object
-            
-            LocalDateTime newDateTimeOfApp = MainAbsScheduling.getConvStringToDateTime(cmbDayIn.getSelectedItem().toString() + cmbMonthin.getSelectedItem().toString() +cmbYearIn.getSelectedItem().toString() + cmbTimeIn.getSelectedItem().toString());
-         
-            //check the proposed date and time in the Docotrs available list
-            
-            //this means that the Doctor has this appointment available, so the change can go ahead
-            if (currentDoctorInst.getDocsAvailableAppointments().contains(newDateTimeOfApp)){
-                        //remove the appointment time from the doctors appointment times taken list
-                   currentDoctorInst.setRemoveLDTFromAppTimesTakenList(currentDateTimeOfInst);
+        //this means there is a change to make as all criteria for change have been met
+        else{
+                        //this means that the date and time has changed, it is amore complicated routine involving more objects
+                        if (amendAppFormInst.getFormUpdatedDateTime() == true){
+              
+                           //this means that the Doctor has this appointment available, so the change can go ahead
+                           if (currentDoctorInst.getDocsAvailableAppointments().contains(newDateTimeOfApp)){
+                                       //remove the appointment time from the doctors appointment times taken list
+                                  currentDoctorInst.setRemoveLDTFromAppTimesTakenList(currentDateTimeOfInst);
 
-                   //put the appointment back in their available appointment lists
-                   currentDoctorInst.setAddLDTToAppTimesAvailableList(currentDateTimeOfInst);
-                   
-                   
-                   //remove the new time from the doctor's available list
-                   currentDoctorInst.setRemoveLDTFromAppTimesAvailableList(newDateTimeOfApp);
+                                  //put the appointment back in their available appointment lists
+                                  currentDoctorInst.setAddLDTToAppTimesAvailableList(currentDateTimeOfInst);
 
-                   //add the new appointment time to the doctors appointments taken time list
-                   currentDoctorInst.setAddLDTToAppTimesTakenList(newDateTimeOfApp);
-                   
-                   //set the appointment's date and time to the new date and time
-                   currentAppointmentInst.setAppDateTime(newDateTimeOfApp);
-                   
-                   callPromptAppupdated();
-                   
-            }
-            
-            //this means the appointment is not in the doctor's available appointments list, so the change can't go ahead
-            else{
-                JOptionPane.showMessageDialog (null,
-                "The doctor is busy on this date and time, please select an alternative date",
-                "Appointment date & time Not available",
-                JOptionPane.ERROR_MESSAGE);
-            }
+
+                                  //remove the new time from the doctor's available list
+                                  currentDoctorInst.setRemoveLDTFromAppTimesAvailableList(newDateTimeOfApp);
+
+                                  //add the new appointment time to the doctors appointments taken time list
+                                  currentDoctorInst.setAddLDTToAppTimesTakenList(newDateTimeOfApp);
+
+                                  //set the appointment's date and time to the new date and time
+                                  currentAppointmentInst.setAppDateTime(newDateTimeOfApp);
+
+
+
+                           }
+
+                    }
             
             
-                //Change appointment date and time
-               //change the appointment date and time
-                //update Doctors appointment schedule – take out old appointment
-                //take out appointment from their taken list
-                //put it back in their available list
-                //update Doctors appointment schedule – book new appointment
-                //put new time and date in in their taken list
-                //take out appointment from their available list
-                //*no new appointment made, original stays on file – no other needs to be created, just update this appointment object
-       
         
+                //if the form has been altered in any way, one of two variables will be set at true. One involves teh date and time being 
+                //interacted with which means more objects are involved and is more complicated. One involves just the appointment
+                //object being interacted with
+
+                //program needs updating because somethign on the form other than the date and time has been updated
+                //this means only the appointment needs updating
+                if (amendAppFormInst.getFormUpdatedNotDateTime() == true){
+         
+                     //set appointment attended status
+                    currentAppointmentInst.setAppAttendSec(attendedIn.isSelected());
+
+                    //set appointment missed status
+                    currentAppointmentInst.setAppMissed(missedIn.isSelected());
+                    //set any new symptoms
+                    currentAppointmentInst.setSymptoms(symptomsIn.getText());
+
+                  //put the appointment back in the map
+                   // MainAbsAppointmentComponent.setPutInMap(AppKeyIn.getText(), currentAppointmentInst);
+
+
+                }
+        
+        
+        
+                //this means the app has been cancelled, it needs to be marked as cancelled, and the doctor's schdule lists
+                //need updating (the appointment time taking out of theri taken list, and putting back in the available
+                //list
+                if(amendAppFormInst.getFormUpdatedNotDateTimeButAppAndDoc() == true){
+
+
+                      //set appointment cancelled status
+                    currentAppointmentInst.setAppCancelled(cancelledIn.isSelected());
+
+                    //get the date and time of appointment as an object
+                   currentDateTimeOfInst = currentAppointmentInst.getAPPDateAndTime();
+
+                    //remove the appointment time from the doctors appointment time staken list
+                    currentDoctorInst.setRemoveLDTFromAppTimesTakenList(currentDateTimeOfInst);
+
+                    //add the appointment time to the doctors appointments available list
+                    currentDoctorInst.setAddLDTToAppTimesAvailableList(currentDateTimeOfInst);
+
+                    //*appointment still stays on file so no further action
+
+                }
+  
+                
+                //output message to tell you that the appointment has been updated
+                callPromptAppupdated();
+                 //go back to patient record screen
+                JFrameSecretaryMenu SecMenuRef = JFrameSecretaryMenu.getInstance();
+                SecMenuRef.setSecSouthBorderString("DisplayPatientDetails");
+                
             }
         
-                //variables check whether the form has been updated or not set back to false for a new update
-                    amendAppFormInst.setFormUpdatedNotDateTime(false);
-                    amendAppFormInst.setFormUpdatedDateTime(false);
-                    amendAppFormInst.setFormUpdatedNotDateTimeButAppAndDoc(false);
+            //variables check whether the form has been updated or not set back to false for a new update
+                amendAppFormInst.setFormUpdatedNotDateTime(false);
+                amendAppFormInst.setFormUpdatedDateTime(false);
+                amendAppFormInst.setFormUpdatedNotDateTimeButAppAndDoc(false);
 
                     
                     
